8321de0f41cd03e8ff284f7ddef1ffb71ac14e84
NikhileshDev <nikhilesh.thakur@grootsoftwares.com>
2024-12-17 19:01:39 +0530

    Layout, Search and Filter task is Ready

--- src/App.js
+++ src/App.js
@@ -1,16 +1,125 @@
-
+import React, { useState, useEffect } from 'react';
+import { Input, Space, Button, Row, Col, Breadcrumb, Layout, theme, Dropdown } from 'antd';
+import { FormOutlined, DownOutlined } from '@ant-design/icons';
 import './App.css';
+import { } from 'antd';
 
-function App() {
+const { Header, Content, Footer } = Layout;
+const { Search } = Input;
+const onSearch = (value, _e, info) => console.log(info?.source, value);
+
+const items = [
+  {
+    key: '1',
+    label: (
+      <a target="_blank" rel="noopener noreferrer" >
+        ALL
+      </a>
+    ),
+  },
+
+  {
+    key: '2',
+    label: (
+      <a target="_blank" rel="noopener noreferrer" >
+        Active
+      </a>
+    ),
+  },
+
+  {
+    key: '3',
+    label: (
+      <a target="_blank" rel="noopener noreferrer" >
+        Completed
+      </a>
+    ),
+    disabled: true,
+  },
+];
+
+
+const App = () => {
+  const [tasks, setTasks] = useState();
+  const [newTask, setNewTask] = useState();
+
+  const {
+    token: { colorBgContainer, borderRadiusLG },
+  } = theme.useToken();
+
+  const addTask = () => { }
   return (
-    <div className='main' style={{minHeight:window.innerHeight, maxHeight:window.innerHeight}}>
-      <h1>Task Management App</h1>
-      <div className="App">
-      
-      <div className='center'>Task 1  </div>
-    </div>
+    <div className='main-div'>
+
+      <Layout>
+        <Header
+          style={{
+            display: 'flex',
+            alignItems: 'center',
+          }}
+        >
+
+
+        </Header>
+        <Content
+          style={{
+            padding: '0 48px',
+          }}
+        >
+          <Breadcrumb
+            style={{
+              margin: '16px 0',
+            }}
+          >
+            <Breadcrumb.Item>Home</Breadcrumb.Item>
+            <Breadcrumb.Item>List</Breadcrumb.Item>
+            <Breadcrumb.Item>App</Breadcrumb.Item>
+          </Breadcrumb>
+
+          <Space direction='vertical'>
+            <Search
+              placeholder='input search text'
+              enterButton='Search'
+              onSearch={onSearch}
+              size='large'
+            />
+          </Space>
+
+          <Dropdown
+            menu={{
+              items,
+            }}
+          >
+            <a onClick={(e) => e.preventDefault()}>
+              <Space>
+                <Button type='primary'>
+                  Select<DownOutlined />
+                </Button>
+              </Space>
+            </a>
+          </Dropdown>
+          <div
+            style={{
+              background: colorBgContainer,
+              minHeight: 500,
+              padding: 24,
+              borderRadius: borderRadiusLG,
+              marginTop: 10
+            }}
+          >
+            Tasks <FormOutlined />
+          </div>
+        </Content>
+        <Footer
+          style={{
+            textAlign: 'center',
+          }}
+        >
+          An Design ©{new Date().getFullYear()}
+        </Footer>
+      </Layout>
     </div>
   );
-}
+};
 
 export default App;
